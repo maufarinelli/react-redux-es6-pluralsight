bc0c668734a463935c2673cfde4db456740b21dc
Mauricio Farinelli <maufarinelli@gmail.com>
2017-09-07 19:29:26 -0400

    until create Load Courses Thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,16 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
-  return {type: types.CREATE_COURSE, course};
+export function loadCoursesSuccess(courseS) {
+  return {type: types.LOAD_COURSES_SUCCESS, courseS};
+}
+
+export function loadCourses() {
+  return function loadCourses(dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  }
 }
